25283ebe6ad50fc61645baa00bcb2f35fd743335
tugalsan karabacak <tugalsan@gmail.com>
2022-06-20 23:39:26 +0300

    Initial commit

---
+++ src/main/java/com/tugalsan/api/sql/order/server/TS_SQLOrder.java
@@ -0,0 +1,24 @@
+package com.tugalsan.api.sql.order.server;
+
+import com.tugalsan.api.list.client.*;
+import com.tugalsan.api.sql.sanitize.server.*;
+import com.tugalsan.api.string.client.*;
+import java.util.List;
+
+public class TS_SQLOrder {
+
+    public TS_SQLOrder(boolean ascending) {
+        this.ascending = ascending;
+    }
+    public List<String> columnNames = TGS_ListUtils.of();
+    public boolean ascending = true;
+
+    @Override
+    public String toString() {
+        if (columnNames.isEmpty()) {
+            return "";
+        }
+        TS_SQLSanitizeUtils.sanitize(columnNames);
+        return TGS_StringUtils.concat("ORDER BY ", TGS_StringUtils.toString(columnNames, ","), ascending ? " ASC" : " DESC");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module com.tugalsan.api.sql.order {
+    requires com.tugalsan.api.string;
+    requires com.tugalsan.api.list;
+    requires com.tugalsan.api.sql.sanitize;
+    exports com.tugalsan.api.sql.order.server;
+}
